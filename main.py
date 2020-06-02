72f02a7979c18ae030c072aceee254de5d2f7b0f
Alpha0081 <47236980+Alpha0081@users.noreply.github.com>
2020-06-02 10:52:39 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import sys
 from GUI.mainwindow import MainWindow
 from PyQt5.QtWidgets import QApplication
+from dev.config import Config
 
+d = Config()
+print(d.language)
 app = QApplication(sys.argv)
 ex = MainWindow()
 sys.exit(app.exec_())
