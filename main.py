5715f142f03e6c1730f403d5841ef9ffc4509b1f
Artem Mikhailov <artem.mihaylov.2017@gmail.com>
2021-07-27 19:08:15 +0300

    изменено:      main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from PyQt5.QtWidgets import QApplication
 from view.mainwindow import MainWindow
 from models.config import config
 
-print("OK")
 app = QApplication(sys.argv) 
 ex = MainWindow()
 sys.exit(app.exec_())
